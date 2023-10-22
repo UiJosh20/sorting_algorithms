4f5ce8497eb6fe7e3269b195bbad0746e34a9cf9
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-10-22 09:25:55 -0700

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,34 +1,81 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- * @list: Pointer to the head of the list
+ * _swap - Swaps two nodes of doubly linked list
+ *
+ * @node: node base to change
+ * @list: double link list head
+ *
+ * Return: No Return
+ */
+void _swap(listint_t **node, listint_t **list)
+{
+	listint_t *temp = *node, *temp2, *temp3;
+
+	if (!(*node)->prev)
+		*list = (*node)->next;
+
+	temp = temp3 = *node;
+	temp2 = temp->next;
+
+	temp->next = temp2->next;
+	temp3 = temp->prev;
+	temp->prev = temp2;
+	temp2->next = temp;
+	temp2->prev = temp3;
+
+	if (temp2->prev)
+		temp2->prev->next = temp2;
+
+
+	if (temp->next)
+		temp->next->prev = temp;
+
+	*node = temp2;
+
+}
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ *
+ * @list: doubly linked list
+ *
+ * Return: No Return
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr = (*list)->next;
-	listint_t *prev = curr->prev;
-	listint_t *next = curr->next;
+	listint_t  *head = *list, *tback, *aux;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (!head || (!(head->prev) && !(head->next)))
 		return;
 
-	while (curr)
+	while (head && head->next)
 	{
-		while (prev && prev->n > curr->n)
+		if (head->n > head->next->n)
 		{
-			if (prev->prev)
-				prev->prev->next = curr;
-			else
-				*list = curr;
-			curr->prev = prev->prev;
-			prev->next = next;
-			if (next)
-				next->prev = prev;
-			curr->next = prev;
-			prev->prev = curr;
+			aux = head;
+
+			_swap(&aux, list);
 			print_list(*list);
+			head = aux;
+			tback = aux;
+
+			while (tback && tback->prev)
+			{
+
+				if (tback->n < tback->prev->n)
+				{
+					aux = tback->prev;
+
+					_swap(&(aux), list);
+
+					print_list(*list);
+					tback = aux->next;
+				}
+
+				tback = tback->prev;
+			}
+
 		}
-		curr = next;
+		head = head->next;
 	}
 }
